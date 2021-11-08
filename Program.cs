646204a709b209b0c4e26175aa2f025202934cb1
Ekaterina-Ryzhkova <71314783+Ekaterina-Ryzhkova@users.noreply.github.com>
2021-11-08 16:53:49 +0500

    secondattempt

--- Program.cs
+++ Program.cs
@@ -4,43 +4,19 @@ namespace visual_studio_code_bin
 {
     class Program
     {
-        static bool x;
         public static void Main(string[] args)
         {
-           
- Console.WriteLine(IsLeapYear(2014));
- Console.WriteLine(IsLeapYear(1999));
- Console.WriteLine(IsLeapYear(8992));
- Console.WriteLine(IsLeapYear(1));
- Console.WriteLine(IsLeapYear(14));
- Console.WriteLine(IsLeapYear(400));
- Console.WriteLine(IsLeapYear(600));
- Console.WriteLine(IsLeapYear(3200));
- FinalTesting(IsLeapYear(Convert.ToInt32(x))); // Idk
+            int[] years = { 2014, 1999, 8992, 1, 14, 400, 600, 3200 };
 
-}
-
-public static bool IsLeapYear(int year)
-
-{
-    x = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-    return x;
-
-}
-
-public static void FinalTesting(bool IsLeapYear)
-
-{
-
-    for (int i = Convert.ToInt32(IsLeapYear); i < 7; i++) { 
-        
-if (i == 0) {
-        Console.WriteLine("Год {0} високосный", i);
-    } else Console.WriteLine("Год {0} невисокосный", i);
-}
-}
-    
-        
-    }
+            foreach (int n in years)
+            {
+                if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0))
+                {
+                    Console.WriteLine("Год {0} високосный", n);
+                }
+                else Console.WriteLine("Год {0} НЕвисокосный", n);
+            }
+        }
+    } //Форматирование CTRL+SHIFT+I
 }
 
