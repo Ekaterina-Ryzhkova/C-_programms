0c131d46e16e98f04a7028060d95e87c188e8b83
Ekaterina-Ryzhkova <71314783+Ekaterina-Ryzhkova@users.noreply.github.com>
2021-11-12 17:05:02 +0500

    find min value

--- Program.cs
+++ Program.cs
@@ -1,22 +1,75 @@
 ﻿using System;
 
-namespace visual_studio_code_bin
+namespace Hello
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
-            int[] years = { 2014, 1999, 8992, 1, 14, 400, 600, 3200 };
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Enter the number of three pairs");
+            int rows = int.Parse(Console.ReadLine()); //строки
+            int columns = 3; //столбцы
+            int[,] mas = new int[rows, columns];
+            string[] str_arr;
 
-            foreach (int n in years)
-            {
-                if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0))
+            int k = 0; //7 3 5 15 20 40 300 550 137
+
+            str_arr = (Console.ReadLine()).Split(' ');
+
+            for (int i = 0; i < rows; ++i)
                 {
-                    Console.WriteLine("Год {0} високосный", n);
+                    for (int j =0; j<columns ;j++) {
+                    Console.WriteLine("{0} {1} {2}",i,j,str_arr[k]);
+                    mas[i, j] = Convert.ToInt32(str_arr[k]);
+                    k++;
+                    }
                 }
-                else Console.WriteLine("Год {0} НЕвисокосный", n);
-            }
-        }
-    } //Форматирование CTRL+SHIFT+I
-}
 
+            Console.WriteLine("Полученная матрица:");
+            for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    Console.Write(String.Format("{0,5}", mas[i, j]));
+                    Console.WriteLine();
+                }
+
+            int minValue = 0; 
+            Console.WriteLine("min " + minValue);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {   
+                    if (j==2)
+                    {
+                        minValue = mas[i,j];
+                        if ((minValue > mas[i,j-1]) && (minValue > mas[i,j-2]))
+                        {
+                            minValue = mas[i,j-1];
+                            if (minValue > mas[i,j-2])
+                            {
+                                minValue = mas[i,j-2];
+                                Console.Write($"{minValue} ");
+                            }
+                            else
+                            {
+                                minValue = mas[i,j-1];
+                                Console.Write($"{minValue} ");
+                            }
+                        }else
+                        {
+                            minValue = mas[i,j];
+                            Console.Write($"{minValue} ");
+                        }
+
+                        minValue = 0;
+                    }
+                    
+                } 
+            }
+            
+        } 
+    }
+} 
+
+            
+        
